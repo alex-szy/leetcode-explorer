62ab9e7ed4aeee8ee3e68315006d831ae90d8448
Alex Su <suziyue@gmail.com>
2025-04-14 18:05:32 -0700

    Support for languages other than python

--- scripts/generate_site.py
+++ scripts/generate_site.py
@@ -1,5 +1,6 @@
 import os
 from jinja2 import Template
+from typing import Dict
 
 SOLUTION_DIR = "solutions"
 OUTPUT_DIR = "build"
@@ -7,48 +8,69 @@ TEMPLATE_FILE = "scripts/template.html"
 OUTPUT_FILE = os.path.join(OUTPUT_DIR, "index.html")
 
 
-def parse_metadata(file_path):
+def parse_metadata(file_path: str) -> Dict:
     metadata = {
-        "filename": os.path.basename(file_path),
-        "code": "",
-        "title": "",
-        "link": "",
-        "difficulty": "",
-        "id": "",
-        "tags": []
+        "Title": "",
+        "Link": "",
+        "Difficulty": "",
+        "ID": "",
+        "Tags": []
     }
-    with open(file_path, "r", encoding="utf-8") as f:
-        lines = f.readlines()
+
+    COMMENT_STR = {
+        "java": "//",
+        "c": "//",
+        "cpp": "//",
+        "py": "#"
+    }
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+    except:
+        print("Error parsing file:", file_path, "- skipped")
+        return None
+
+    ext = file_path.split(".")[-1]
+    if ext not in COMMENT_STR:
+        print("Unsupported file:", file_path, "- skipped")
+        return None
+
+    c = COMMENT_STR[ext]
 
     start, end = None, None
     for i, line in enumerate(lines):
-        if line.startswith("# ID:"):
-            metadata["id"] = line.split(":", 1)[1].strip()
-        if line.startswith("# Title:"):
-            metadata["title"] = line.split(":", 1)[1].strip()
-        elif line.startswith("# Link:"):
-            metadata["link"] = line.split(":", 1)[1].strip()
-        elif line.startswith("# Difficulty:"):
-            metadata["difficulty"] = line.split(":", 1)[1].strip()
-        elif line.startswith("# Tags:"):
-            metadata["tags"] = [tag.strip()
-                                for tag in line.split(":", 1)[1].split(",")]
-        elif not line.startswith("#") and start is None:
+        if line.startswith(c):
+            fields = line.removeprefix(c).lstrip().split(":", 1)
+            if len(fields) != 2:
+                continue
+            field, data = fields
+            template = metadata.get(field)
+            if isinstance(template, list):
+                metadata[field] = [tag.strip()
+                                   for tag in data.split(",")]
+            elif isinstance(template, str):
+                metadata[field] = data.strip()
+        elif line.startswith("class Solution") or start is None:
             start = i
         elif line.startswith("if __name__"):
             end = i
             break
 
-    metadata["code"] = "".join(lines[start:end])
+    for value in metadata.values():
+        if not value:
+            return None
+
+    metadata["code"] = "".join(lines[start:end]).strip()
     return metadata
 
 
 def main():
     entries = []
     for filename in os.listdir(SOLUTION_DIR):
-        if filename.endswith(".py") and filename != "00-template.py":
-            path = os.path.join(SOLUTION_DIR, filename)
-            entries.append(parse_metadata(path))
+        path = os.path.join(SOLUTION_DIR, filename)
+        metadata = parse_metadata(path)
+        metadata and entries.append(metadata)
 
     with open(TEMPLATE_FILE, "r", encoding="utf-8") as f:
         template = Template(f.read())
